256368c472bdc9c73e397f0f4e5c5ea5a5afd90b
Stephen Brown II <Stephen.Brown2@gmail.com>
2015-01-01 05:21:35 -0600

    Whoops, forgot the javascript for the Nav Menu

--- src/render/scripts/script.js
+++ src/render/scripts/script.js
@@ -1,1 +1,32 @@
 /* Your scripts go here */
+
+// Internet Defense League Alert
+    window._idl = {};
+    _idl.variant = "modal";
+    (function() {
+        var idl = document.createElement('script');
+        idl.type = 'text/javascript';
+        idl.async = true;
+        idl.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'modal');
+        document.getElementsByTagName('body')[0].appendChild(idl);
+    })();
+
+// Pull Nav function for mobile menu
+    $(function() {
+      var pull    = $('#pull');
+        menu    = $('nav ul');
+        menuHeight  = menu.height();
+
+      $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+      });
+
+      $(window).resize(function(){
+            var w = $(window).width();
+            if(w > 320 && menu.is(':hidden')) {
+              menu.removeAttr('style');
+            }
+        });
+    });
+
