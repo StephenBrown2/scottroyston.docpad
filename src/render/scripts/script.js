1d72d882791fbd133b44df7a2e7b33f03a62e59c
Stephen Brown II <Stephen.Brown2@gmail.com>
2015-06-14 23:39:24 -0500

    First stab. Array is updated properly, but clicking elements opens MFP at 0th element

--- src/render/scripts/script.js
+++ src/render/scripts/script.js
@@ -1,115 +1,137 @@
 /* Your scripts go here */
 
 // Pull Nav function for mobile menu
-    $(function() {
-      var pull    = $('#pull');
-        menu    = $('nav ul');
-        menuHeight  = menu.height();
+$(function() {
+    var pull = $('#pull');
+    menu = $('nav ul');
+    menuHeight = menu.height();
 
-      $(pull).on('click', function(e) {
+    $(pull).on('click', function(e) {
         e.preventDefault();
         menu.slideToggle();
-      });
-
-      $(window).resize(function(){
-            var w = $(window).width();
-            if(w > 320 && menu.is(':hidden')) {
-              menu.removeAttr('style');
-            }
-        });
     });
+
+    $(window).resize(function() {
+        var w = $(window).width();
+        if (w > 320 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+});
 
 // Legacy Isotope JS
-    var $oldcontainer = $('.oldisotope');
+var $oldcontainer = $('.oldisotope');
 
-    $oldcontainer.isotope({
-        itemSelector: '.oldart',
-        masonry: {
-            columnWidth: 250,
-        },
-        sortBy: 'sdate',
-        sortAscending: false,
-        getSortData: {
-            sdate: '[data-sort]',
-        },
-    });
+$oldcontainer.isotope({
+    itemSelector: '.oldart',
+    masonry: {
+        columnWidth: 250,
+    },
+    sortBy: 'sdate',
+    sortAscending: false,
+    getSortData: {
+        sdate: '[data-sort]',
+    },
+});
 
-// Isotope JS
-    var $container = $('.isotope'),
-        qsRegex,
-        filters = {};
-
-    $container.isotope({
-        itemSelector: '.art',
-        masonry: {
-            columnWidth: '.grid-sizer',
-            gutter: '.gutter-sizer',
-        },
-        sortBy: 'sdate',
-        sortAscending: false,
-        getSortData: {
-            title: '[data-title]',
-            sdate: '[data-sort-date]',
-        },
-        isInitLayout: false
-    });
-
-    // use value of search field to filter
-    var $quicksearch = $('#quicksearch').keyup(debounce(searchFilter));
-
-    function searchFilter() {
-        qsRegex = new RegExp($quicksearch.val(), 'gi');
-        $container.isotope({
-            filter: function () {
-                return qsRegex ? $(this).text().match(qsRegex) : true;
-            }
+$oldcontainer.isotope('on', 'layoutComplete',
+    function(laidOutItems) {
+        filtered = laidOutItems.filteredItems;
+        var elems = [];
+        for (var i = 0; i < filtered.length; i++) {
+            item = {
+                src: filtered[i].element.getElementsByTagName('a')[0].href,
+                title: filtered[i].element.getElementsByTagName('a')[0].title
+            };
+            $(filtered[i].element).attr('onclick', 'javascript:mfp.goTo('+i+');');
+            elems.push(item);
+        }
+        var mfp = $('.oldwrapper a.old').magnificPopup({
+            items: elems,
+            type: 'image',
+            gallery: {
+                enabled: true,
+                preload: [1, 2],
+            },
         });
     }
+);
 
+var mfp = $('.oldwrapper a.old').magnificPopup.instance
 
-    function debounce(fn, threshold) {
-        var timeout;
-        return function debounced() {
-            if (timeout) {
-                clearTimeout(timeout);
-            }
-
-            function delayed() {
-                fn();
-                timeout = null;
-            }
-            setTimeout(delayed, threshold || 100);
-        }
-    }
-
-    $('#filters').on( 'click', 'button', function() {
-      var filterValue = $(this).attr('data-filter');
-        $container.isotope({ filter: filterValue });
-    });
+$oldcontainer.isotope('layout');
 
 // magnificPopup JS
-    $('.wrapper').magnificPopup({
-        delegate: 'a.overlay:visible',
-        type:'image',
-        image: {
-            titleSrc: 'data-title',
-        },
-        gallery: {
-            enabled: false,
-            preload: [1,2],
-        },
-    });
+$('.wrapper').magnificPopup({
+    delegate: 'a.overlay:visible',
+    type: 'image',
+    image: {
+        titleSrc: 'data-title',
+    },
+    gallery: {
+        enabled: false,
+        preload: [1, 2],
+    },
+});
 
-    $('.oldwrapper a.old').magnificPopup({
-        type:'image',
-        gallery: {
-            enabled: true,
-            preload: [1,2],
-        },
-    });
+// Isotope JS
+var $container = $('.isotope'),
+    qsRegex,
+    filters = {};
 
-$(window).load(function(){
-   $container.isotope({
-      isInitLayout: true
-   });
+$container.isotope({
+    itemSelector: '.art',
+    masonry: {
+        columnWidth: '.grid-sizer',
+        gutter: '.gutter-sizer',
+    },
+    sortBy: 'sdate',
+    sortAscending: false,
+    getSortData: {
+        title: '[data-title]',
+        sdate: '[data-sort-date]',
+    },
+    isInitLayout: false
+});
+
+// use value of search field to filter
+var $quicksearch = $('#quicksearch').keyup(debounce(searchFilter));
+
+function searchFilter() {
+    qsRegex = new RegExp($quicksearch.val(), 'gi');
+    $container.isotope({
+        filter: function() {
+            return qsRegex ? $(this).text().match(qsRegex) :
+                true;
+        }
+    });
+}
+
+
+function debounce(fn, threshold) {
+    var timeout;
+    return function debounced() {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+
+        function delayed() {
+            fn();
+            timeout = null;
+        }
+        setTimeout(delayed, threshold || 100);
+    };
+}
+
+$('#filters').on('click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $container.isotope({
+        filter: filterValue
+    });
+});
+
+$(window).load(function() {
+    $container.isotope({
+        isInitLayout: true
+    });
 });
